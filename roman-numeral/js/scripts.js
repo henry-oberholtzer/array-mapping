9026fa1bb7dd895c1c3946729e2f53a3bb021496
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-10-26 16:19:39 -0700

    clean code for roman numerals. currently solved up to 39
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- roman-numeral/js/scripts.js
+++ roman-numeral/js/scripts.js
@@ -11,20 +11,17 @@ function make1to8(inputNum) {
 }
 // currently working 9-39!!!
 function make9to19(inputNum) {
-    const X = "X";
-    if (inputNum > 30) {
+    if (inputNum < 40 && inputNum % 10 === 0) {
+        return "X".repeat(inputNum/10)
+    } else if (inputNum > 30) {
         return "XX" + make9to19(inputNum % 20)
-    } else if (inputNum === 30) {
-        return "XXX"
     } else if (inputNum > 20) {
         return "X" + make9to19(inputNum % 20)
-    } else if (inputNum === 20) {
-        return "X" + make9to19(inputNum / 2)
     } else if (inputNum === 9) {
         return "IX";
     } else if (inputNum % 10 < 9) {
-        return X + make1to8(inputNum % 10)
+        return "X" + make1to8(inputNum % 10)
     } else if (inputNum % 10 === 9) {
-        return X + make9to19(inputNum % 10)
+        return "X" + make9to19(inputNum % 10)
     }
 }
